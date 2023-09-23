33a7b79b05279dbc2a0e6146661dca63f6239149
alexticona0802 <alex.ticona@lundero.com>
2023-09-23 18:34:22 -0500

    exploring data from a csv file

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -1,23 +1,69 @@
+import csv
 import json
 
 def lambda_handler(event, context):
     try:
         # Extract the CSV data from the event
         csv_data = event["body"]
-        
         # Parse the CSV data into a list of dictionaries
         csv_list = []
-        csv_reader = csv.DictReader(csv_data.splitlines())
+        csv_reader = csv.reader(csv_data.splitlines())
         for row in csv_reader:
             csv_list.append(row)
         
-        # You now have a list of dictionaries representing the CSV data
-        # You can perform any required transformations or processing here
         
-        # For demonstration purposes, return the list of dictionaries as JSON
+        print('reading csv file')
+        print(csv_list)
+        list_val = csv_list
+        
+        if event['headers']['table'] == 'jobs':
+            print('procesing jobs table')
+            map_data = {
+                "jobs": [
+                    {
+                        "id": int(val[0]),
+                        "job": val[1]
+                    }
+                    for val in list_val
+                ]
+            }
+            
+            print(map_data)
+        
+        if event['headers']['table'] == 'departments':
+            print('procesing departments table')
+            map_data = {
+                "departments": [
+                    {
+                        "id": int(val[0]),
+                        "department": val[1]
+                    }
+                    for val in list_val
+                ]
+            }
+            
+            print(map_data)
+        
+        if event['headers']['table'] == 'hired_employees':
+            print('procesing hired_employees table')
+            map_data = {
+                "hired_employees": [
+                    {
+                        "id": int(val[0]), 
+                        "name": val[1],
+                        "datetime": val[2],
+                        "department_id": None if val[3]=='' else int(val[3]),
+                        "job_id": None if val[4]=='' else int(val[4])
+                    }
+                    for val in list_val
+                ]
+            }
+            
+            print(map_data)
+        
         return {
             "statusCode": 200,
-            "body": json.dumps(csv_list)
+            "body": json.dumps(map_data)
         }
     except Exception as e:
         return {
