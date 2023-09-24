c6501e1ca1cdeaa6ed049bd9157d5dc7590ba1a0
alexticona0802 <alex.ticona@lundero.com>
2023-09-24 10:33:58 -0500

    adding solution of second sql requirement

---
+++ sql/section2_req2.sql
@@ -0,0 +1,17 @@
+-- count of the number of employees hired in 2021 higher than the average ordered descending
+
+SELECT
+  id,
+  department,
+  hired
+FROM
+  (SELECT b_h.*, avg(hired) over() mean
+  FROM
+    (SELECT d.id, d.department, count(e.id) hired
+    FROM  hired_employees e
+    INNER JOIN departments d ON  e.department_id = d.id
+    WHERE year(datetime) = "2021"
+    GROUP BY department) b_h
+  ) b_m
+WHERE hired > mean  
+ORDER BY hired desc
