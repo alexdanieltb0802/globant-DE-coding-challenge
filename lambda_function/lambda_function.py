c362ed0103ce436c0785ad2c6197fd7c5acdbc0d
alexticona0802 <alex.ticona@lundero.com>
2023-09-24 00:21:54 -0500

    inserting data to Db tables

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -38,57 +38,92 @@ def lambda_handler(event, context):
         print(csv_list)
         list_val = csv_list
         
-        if event['headers']['table'] == 'jobs':
-            print('procesing jobs table')
-            map_data = {
-                "jobs": [
-                    {
-                        "id": int(val[0]),
-                        "job": val[1]
-                    }
-                    for val in list_val
-                ]
-            }
-            
-            print(map_data)
-        
-        if event['headers']['table'] == 'departments':
-            print('procesing departments table')
-            map_data = {
-                "departments": [
-                    {
-                        "id": int(val[0]),
-                        "department": val[1]
-                    }
-                    for val in list_val
-                ]
-            }
-            
-            print(map_data)
-        
-        if event['headers']['table'] == 'hired_employees':
-            print('procesing hired_employees table')
-            map_data = {
-                "hired_employees": [
-                    {
-                        "id": int(val[0]), 
-                        "name": val[1],
-                        "datetime": val[2],
-                        "department_id": None if val[3]=='' else int(val[3]),
-                        "job_id": None if val[4]=='' else int(val[4])
-                    }
-                    for val in list_val
-                ]
-            }
-            
-            print(map_data)
+        tb_content = map_csv_content(event,list_val)
+        insert_toDB(tb_content)
         
         return {
             "statusCode": 200,
-            "body": json.dumps(map_data)
+            "body": json.dumps('succeed!')
         }
     except Exception as e:
         return {
             "statusCode": 500,
             "body": f"Error: {str(e)}"
     }
+
+def map_csv_content(event,registros):
+    map_data={}
+    if event['headers']['table'] == 'jobs':
+        print('procesing jobs table')
+        map_data = {
+            "jobs": [
+                {
+                    "id": int(val[0]),
+                    "job": val[1]
+                }
+                for val in registros
+            ]
+        }
+        
+        print(map_data)
+    
+    if event['headers']['table'] == 'departments':
+        print('procesing departments table')
+        map_data = {
+            "departments": [
+                {
+                    "id": int(val[0]),
+                    "department": val[1]
+                }
+                for val in registros
+            ]
+        }
+        
+        print(map_data)
+    
+    if event['headers']['table'] == 'hired_employees':
+        print('procesing hired_employees table')
+        map_data = {
+            "hired_employees": [
+                {
+                    "id": int(val[0]), 
+                    "name": val[1],
+                    "datetime": val[2],
+                    "department_id": 'NULL' if val[3]=='' else int(val[3]),
+                    "job_id": 'NULL' if val[4]=='' else int(val[4])
+                }
+                for val in registros
+            ]
+        }
+        
+        print(map_data)
+    return map_data
+
+def insert_toDB(tb_content):
+    #inserting data from departments:
+    if 'departments' in tb_content:
+        query_departments = "INSERT INTO departments(id,department) VALUES "
+        for deparment in tb_content['departments']:
+            query_departments+=f"({deparment['id']},'{deparment['department']}'),"
+        query_departments = query_departments[:-1]
+        with conn.cursor() as cur:
+            cur.execute(query_departments)
+        conn.commit()
+    ##Insertamos data de jobs
+    if 'jobs' in tb_content:
+        query_jobs = "INSERT INTO jobs(id,job) VALUES "
+        for job in tb_content['jobs']:
+            query_jobs+=f"({job['id']},'{job['job']}'),"
+        query_jobs = query_jobs[:-1]
+        with conn.cursor() as cur:
+            cur.execute(query_jobs)
+        conn.commit()
+    ##Insertamos data de hired_employees
+    if 'hired_employees' in tb_content:
+        query_employee = "INSERT INTO hired_employees(id,name,datetime,department_id,job_id) VALUES "
+        for employee in tb_content['hired_employees']:
+            query_employee+=f"({employee['id']},'{employee['name']}','{employee['datetime']}',{employee['department_id']},{employee['job_id']}),"
+        query_employee = query_employee[:-1]
+        with conn.cursor() as cur:
+            cur.execute(query_employee)
+        conn.commit()
