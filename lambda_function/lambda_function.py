f5ba947e9e0e956ad67c82cee5b65ef6d23fb02c
alexticona0802 <alex.ticona@lundero.com>
2023-09-23 21:22:23 -0500

    setting database connection from lambda function

--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -1,5 +1,27 @@
 import csv
 import json
+import sys
+import logging
+import rds_config
+import pymysql
+
+rds_host  = "globantdb.cnpqon3b6hn0.us-east-1.rds.amazonaws.com"
+name = rds_config.db_username
+password = rds_config.db_password
+db_name = rds_config.db_name
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(host=rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except pymysql.MySQLError as e:
+    logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
+    logger.error(e)
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
 def lambda_handler(event, context):
     try:
